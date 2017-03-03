751f436287b92a6606ba3a112240fb26f30762bd
HongMichael <michaeltaohong@gmail.com>
2017-03-03 15:50:13 -0500

    work in progress for Users, login broke, need to fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'welcome/index'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#new'
+  delete '/logout', to: 'sessions#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
   resources :tasks
 
   root 'welcome#index'
